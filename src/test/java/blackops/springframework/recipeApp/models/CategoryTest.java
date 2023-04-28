160b006b96086fa72a35ecb652466d01e58c923f
shaiedri <shaiedri850@gmail.com>
2023-04-28 09:03:49 +0300

    Create and Update a Recipe Closes #6 Closes #7

---
+++ src/test/java/blackops/springframework/recipeApp/models/CategoryTest.java
@@ -0,0 +1,28 @@
+package blackops.springframework.recipeApp.models;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CategoryTest {
+    Category category;
+    @BeforeEach
+    public void setUp(){
+        category = new Category();
+    }
+    @Test
+    void getId() {
+        Long id = 4L;
+        category.setId(id);
+        assertEquals(id, category.getId());
+      }
+
+    @Test
+    void getDescription() {
+      }
+
+    @Test
+    void getRecipes() {
+      }
+}
